e26a9c196ece8bb6fcf6e05df1f6ac43871b97f0
divin17 <divinfiston1@gmail.com>
2019-06-23 22:10:01 +0200

    recent updates II

--- Classes/mySqlConnection.php
+++ Classes/mySqlConnection.php
@@ -4,13 +4,11 @@ include 'Connection.php';
 class mySqlConnection extends Connection
 {
     require '../models/config.php';
-    protected $conn;
     public function connect()
     {
         try {
             $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
-            $this->conn = new PDO($dsn, $user, $pswd, $options);
-            return $this->conn;
+            return new PDO($dsn, $user, $pswd, $options);
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
